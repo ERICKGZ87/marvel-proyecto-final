f121c44dd309fd26f7bc61ac0928c6a4cd563a1a
ERICKGZ87 <erickgz87@gmail.com>
2021-11-07 01:04:58 +0000

    mejoras home

---
+++ src/components/Carrousel.js
@@ -0,0 +1,69 @@
+
+import PropType from "prop-types";
+
+const Carrousel = ({ item1, item2, item3, item4, item5, item6, item7 }) => {
+
+    return (
+        <div
+            id="carouselExampleInterval"
+            className="carousel slide"
+            data-bs-ride="carousel"
+        >
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval={1000}>
+                    <img src={item1} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item2} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item3} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item4} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item5} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item6} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval={2000}>
+                    <img src={item7} className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev"
+            >
+                <span className="carousel-control-prev-icon" aria-hidden="true" />
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next"
+            >
+                <span className="carousel-control-next-icon" aria-hidden="true" />
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+    )
+
+}
+Carrousel.PropType = {
+    Item1: PropType.string,
+    Item2: PropType.string,
+    Item3: PropType.string,
+    Item4: PropType.string,
+    Item5: PropType.string,
+    Item6: PropType.string,
+    Item7: PropType.string,
+}
+
+export default Carrousel;
--- src/vistas/Home.js
+++ src/vistas/Home.js
@@ -1,3 +1,4 @@
+import Carrousel from "../components/Carrousel";
 
 const Home = () => {
 
@@ -7,11 +8,19 @@ const Home = () => {
 
 <div className="row">
     <div className="col-md-1"></div>
-    <h1>SUperHeroes Mas Poderosos del mundo</h1>
-    <div className="col-md-10">
-
    
-  
+    <div className="col-md-10">
+   
+  <Carrousel
+      item1="https://phantom-marca.unidadeditorial.es/838d5234c7803b705c60abc334975919/resize/1320/f/jpg/assets/multimedia/imagenes/2021/07/06/16255683165037.jpg"
+      item2="https://www.altonivel.com.mx/wp-content/uploads/2018/05/avengers.jpg"
+      item3="https://i.blogs.es/90e2ab/guardians0/1366_2000.jpeg"
+      item4="https://i.blogs.es/26abe2/1366_2000/450_1000.jpeg"
+      item5="https://img.ecartelera.com/noticias/fotos/60400/60446/1.jpg"
+      item6="https://lumiere-a.akamaihd.net/v1/images/keyart_es_ea2a4cef.jpeg?region=420,0,1080,1080&width=960"
+      item7="https://static.dw.com/image/52649694_303.jpg"
+  />
+
    
     </div>
     <div className="col-md-1"></div>
