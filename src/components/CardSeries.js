22220cbcad240ec68fb886159b859a39a65edb1e
plopez1985 <pl586546@gmail.com>
2021-11-07 22:49:45 +0000

    series

---
+++ src/components/CardSeries.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import PropType from "prop-types";
+
+const CardSeries = ({ title,description,fotoChica, id }) => {
+
+    return (
+        <div className="card">
+            <img src={fotoChica} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">
+                  {description}
+                </p>
+                <Link to={"/Series" + id + "/DetalleSeries"} className="btn btn-primary">
+                    Mas Informacion
+                </Link><br/>       
+            </div>
+        </div>
+    )
+
+}
+CardComics.PropType = {
+    descripcion: PropType.string,
+    titulo: PropType.string,
+    id:PropType.number,
+    Url: PropType.string,
+    }
+    
+export default CardSeries;
