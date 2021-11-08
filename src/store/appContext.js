9f688ac1c0f7d17fdf4ea3d0a878254bf1c74605
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-11-08 23:22:45 +0000

    renovacion hash marvel vencido

--- src/store/appContext.js
+++ src/store/appContext.js
@@ -20,9 +20,9 @@ const InjectContext = PassedComponen => {
         }));
         //este useEffect se ejecuta la primera vez q se carga la pagina
         useEffect(() => {
-            state.actions.getData1("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=2c7646b1592cf7f1709584014a70e2b5&hash=e76786c27c33203502f260f6be27384b")
-            state.actions.getData2("https://gateway.marvel.com:443/v1/public/events?ts=1&apikey=2c7646b1592cf7f1709584014a70e2b5&hash=e76786c27c33203502f260f6be27384b")
-            state.actions.getData3("https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=2c7646b1592cf7f1709584014a70e2b5&hash=e76786c27c33203502f260f6be27384b")
+            state.actions.getData1("https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=8a432a54a0c45f83ea1e0ba67bb5e322&hash=8856823601a2b25665d210eabe018e39")
+            state.actions.getData2("https://gateway.marvel.com:443/v1/public/events?ts=1&apikey=8a432a54a0c45f83ea1e0ba67bb5e322&hash=8856823601a2b25665d210eabe018e39")
+            state.actions.getData3("https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=8a432a54a0c45f83ea1e0ba67bb5e322&hash=8856823601a2b25665d210eabe018e39")
         }, [])
 
         return (
