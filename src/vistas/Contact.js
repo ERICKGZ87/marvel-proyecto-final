d4d8cbaa7b4c0b03468f28074da7f891fb2a5e3c
CaballeroVisual <caballerovisual@gmail.com>
2021-11-08 02:19:32 +0000

    marvel-final

--- src/vistas/Contact.js
+++ src/vistas/Contact.js
@@ -3,22 +3,122 @@ const Contact = () => {
 
     return (
         <>
- <div className="container">
+            <div className="container-fluid">
 
-<div className="row">
-    <div className="col-md-1"></div>
-    <h1>Contact</h1>
-    <div className="col-md-10">
+                <div className="row">
+                    <div className="col-md-6" id="contacto"> <h1>Contact</h1></div>
+                    <div className="col-md-6">
+                    
 
-   
-  
-   
-    </div>
-    <div className="col-md-1"></div>
-</div>
+                    <form className="row g-3 needs-validation" noValidate id="contact">
+                        <div className="col-md-4">
+                            <label htmlFor="validationCustom01" className="form-label">
+                                First name
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="validationCustom01"
+                                defaultValue="Mark"
+                                required
+                            />
+                            <div className="valid-feedback">Looks good!</div>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="validationCustom02" className="form-label">
+                                Last name
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="validationCustom02"
+                                defaultValue="Otto"
+                                required
+                            />
+                            <div className="valid-feedback">Looks good!</div>
+                        </div>
+                        <div className="col-md-4">
+                            <label htmlFor="validationCustomUsername" className="form-label">
+                                Username
+                            </label>
+                            <div className="input-group has-validation">
+                                <span className="input-group-text" id="inputGroupPrepend">
+                                    @
+                                </span>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="validationCustomUsername"
+                                    aria-describedby="inputGroupPrepend"
+                                    required
+                                />
+                                <div className="invalid-feedback">Please choose a username.</div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="validationCustom03" className="form-label">
+                                City
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="validationCustom03"
+                                required
+                            />
+                            <div className="invalid-feedback">Please provide a valid city.</div>
+                        </div>
+                        <div className="col-md-3">
+                            <label htmlFor="validationCustom04" className="form-label">
+                                State
+                            </label>
+                            <select className="form-select" id="validationCustom04" required>
+                                <option selected disabled value>
+                                    Choose...
+                                </option>
+                                <option>...</option>
+                            </select>
+                            <div className="invalid-feedback">Please select a valid state.</div>
+                        </div>
+                        <div className="col-md-3">
+                            <label htmlFor="validationCustom05" className="form-label">
+                                Zip
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="validationCustom05"
+                                required
+                            />
+                            <div className="invalid-feedback">Please provide a valid zip.</div>
+                        </div>
+                        <div className="col-12">
+                            <div className="form-check">
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    defaultValue
+                                    id="invalidCheck"
+                                    required
+                                />
+                                <label className="form-check-label" htmlFor="invalidCheck">
+                                    Agree to terms and conditions
+                                </label>
+                                <div className="invalid-feedback">You must agree before submitting.</div>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <button className="btn btn-primary" type="submit">
+                                Submit form
+                            </button>
+                        </div>
+                    </form></div>
 
-</div>
-           
+
+
+                </div>
+
+            </div>
+
         </>
     )
 }
